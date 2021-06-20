b9d32e0bd56c6c2f2bbeb3da828d2cd7eb29ee0f
Valium-K <lschan96@gmail.com>
2021-06-20 20:53:05 +0900

    item_domain

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 생략가능
 @DiscriminatorColumn(name = "dtype")                  // 생략가능
 public abstract class Item {
@@ -26,5 +27,28 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
 
+    // 비지니스 로직
+    // data를 가지고 있는 곳에 서비스를 넣으면 응집도가 증기한다.
+    // 간단한 비지니스 로직은 이렇게 짜는것이 좋다.
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
 
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity) throws NotEnoughStockException {
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
 }
