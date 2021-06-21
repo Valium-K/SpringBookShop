319e191d65e315910eaf296604a79f5fe3847786
Valium-K <lschan96@gmail.com>
2021-06-21 15:21:02 +0900

    생성자 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,10 +1,8 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
-
 import javax.persistence.Embeddable;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
 
 @Embeddable
 @Getter
@@ -12,4 +10,11 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    public Address() {}
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
