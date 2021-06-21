f7c912b6f534e8a7a6a9bdd319e4a5480b4e394e
Valium-K <lschan96@gmail.com>
2021-06-21 15:21:20 +0900

    setter 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 생략가능
 @DiscriminatorColumn(name = "dtype")                  // 생략가능
 public abstract class Item {
